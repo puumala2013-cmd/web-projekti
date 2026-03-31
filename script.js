4732be397b4759a63b0d419dafc10e2353e6d117
puumala2013-cmd <puumala2013@gmail.com>
2026-03-31 15:17:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
+function tehtava1() {
+    document.getElementById("tulos").innerHTML =
+        "Korjattu koodi:<br><pre>function greet(name) {\n  console.log('Hei, ' + name);\n}\n\ngreet('Kimmo');</pre>";
+}
+
+function tehtava2() {
+    let arr = [4, 8, 15, 16, 23, 42];
+    let summa = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        summa += arr[i];
+    }
+
+    let keskiarvo = summa / arr.length;
+
+    document.getElementById("tulos").innerHTML =
+        "Keskiarvo on: " + keskiarvo;
+}
+
+function tehtava3() {
+    let arr = [1, 2, 3, 4, 5, 6];
+    let parilliset = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            parilliset.push(arr[i]);
+        }
+    }
+
+    document.getElementById("tulos").innerHTML =
+        "Parilliset luvut: " + parilliset.join(', ');
+}
+
+function tehtava4() {
+    let arr = ["kissa", "koira", "lintu"];
+    let maara = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        maara += arr[i].length;
+    }
+
+    document.getElementById("tulos").innerHTML =
+        "Merkkien kokonaismäärä: " + maara;
+}
 
